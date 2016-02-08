bf24aa4d7b95b63651da227bfe19e19748779a46
Jon Hilton <hilton.jon@gmail.com>
2016-02-08 21:06:13 +0000

    Now sends email directly via local SMTP server

---
+++ src/AzureStore/Models/ContactUs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureStore.Models
+{
+    public class ContactUsForm
+    {
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string Message { get; set; }
+    }
+}
